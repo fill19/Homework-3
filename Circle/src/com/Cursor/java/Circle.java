0e8f04ef5c5cd780debb3401796ca134669f45cb
Dmytro <65898747+fill19@users.noreply.github.com>
2020-11-21 17:44:41 +0200

    Update Circle.java

--- Circle/src/com/Cursor/java/Circle.java
+++ Circle/src/com/Cursor/java/Circle.java
@@ -1,26 +1,27 @@
 package com.Cursor.java;
 
-public class Circle {
+public class Main {
     public static void main(String[] args) {
-        Circle1 circle = new Circle1();
-        System.out.println("The square of circle = " );
-        circle.Circle1(3);
-    }
-}
-
-class Circle1 {
-    private double radius ;
-
-    public void Circle1(double radius) {
-        this.radius = radius;
-        System.out.println(squareOfCircle());
-    }
-
-    public double squareOfCircle() {
-
-        return (Math.PI * radius * radius);
-
+        Circle circle = new Circle();
+        System.out.println("The square of circle = ");
+        circle.Circle(3);
     }
 
 
+     static class Circle {
+        private double radius;
+
+        public void Circle(double radius) {
+            this.radius = radius;
+            System.out.println(squareOfCircle());
+        }
+
+        public double squareOfCircle() {
+
+            return (Math.PI * radius * radius);
+
+        }
+
+
+    }
 }
